a7fc844444b907e996f7154ed8eefd76d6c6fdbb
Hexulez <henry.juhola@edu.keuda.fi>
2022-04-25 09:34:42 +0300

    laptop to pc

---
+++ StudyPoint/StudyPoint/CONNECT.cs
@@ -0,0 +1,39 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudyPoint
+{
+    internal class CONNECT
+    {
+        //Lisää dataVar muuttujaan mysql database nimi
+        string dataVar;
+
+        private MySqlConnection _connection = new MySqlConnection("datasource=localhost; port=3306;username=root;password=;database="+dataVar+ ";SSL Mode = None");
+
+        public MySqlConnection Connection()
+        {
+            return _connection;
+        }
+
+        public void OpenConnection()
+        {
+            if (_connection.State == ConnectionState.Closed)
+            {
+                _connection.Open();
+            }
+
+        }
+
+        public void CloseConnection()
+        {
+            if (_connection.State == ConnectionState.Open)
+            {
+                _connection.Close();
+            }
+        }
+    }
+}
