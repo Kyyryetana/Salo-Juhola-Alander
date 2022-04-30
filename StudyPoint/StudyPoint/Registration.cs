cad15034a98ddd791d41997e83dec85a666ddcf9
Hexulez <henry.juhola@edu.keuda.fi>
2022-04-30 10:47:44 +0300

    regex beta

--- StudyPoint/StudyPoint/Registration.cs
+++ StudyPoint/StudyPoint/Registration.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Text.RegularExpressions;
 
 
 namespace StudyPoint
@@ -18,12 +19,18 @@ namespace StudyPoint
             string mail = email;
             string password = regPass1;
             string password2 = regPass2;
+            
             USERS users = new USERS();
             if (enimi.Trim().Equals("") || snimi.Trim().Equals("") || mail.Trim().Equals("") || password.Trim().Equals("") || password2.Trim().Equals(""))
             {
                 MessageBox.Show("Kaikki vaaditut kentät tulee olla täytettyinä.");
                 return false;
             }
+            else if (RegCheck(enimi,snimi,mail) == false)
+            {
+                return false;
+            }
+            
             else if (password != password2)
             {
                 MessageBox.Show("Salasanat eivät täsmää.");
@@ -31,6 +38,7 @@ namespace StudyPoint
             }
             else if (users.CheckUser(email))
             {
+
                 MessageBox.Show("Käyttäjätunnus on jo olemassa.");
                 return false;
             }
@@ -43,6 +51,39 @@ namespace StudyPoint
 
 
 
+        }
+
+
+        //regex tarkistaa tiedot
+        private static bool  RegCheck(string name, string lastname, string email)
+        {
+            //string emailPattern = "^[a-zA-Z0-9\._-]{5,25}.@.[a-z]{2,12}.(com|org|co\.in|net|fi)";
+            string emailPattern2 = @"^[\w!#$%&'*+\-/=?\^_`{|}~]+(\.[\w!#$%&'*+\-/=?\^_`{|}~]+)*"
+                + "@"
+                + @"((([\-\w]+\.)+[a-zA-Z]{2,4})|(([0-9]{1,3}\.){3}[0-9]{1,3}))$";
+            Regex emailReg = new Regex(emailPattern2);
+            string namePattern = @"^[a-zA-Z]+[-|\s]?[a-zA-Z]+[\s|-]?[a-zA-Z]+$";
+            Regex nameReg = new Regex(namePattern);
+            
+            if (emailReg.IsMatch(email) == false)
+            {
+                MessageBox.Show("sähköposti osoite ei ole sallittua muotoa");
+                return false;
+            }
+            else if (nameReg.IsMatch(name) == false)
+            {
+                MessageBox.Show("nimi ei ole halutussa muodossa");
+                return false;
+            }
+            else if (nameReg.IsMatch(lastname) == false)
+            {
+                MessageBox.Show("sukunimi ei ole halutussa muodossa");
+                return false;
+            }
+            else
+            {
+                return true;
+            }
         }
 
     }
